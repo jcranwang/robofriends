ad444b65a1eacf7093f79ccf26ce339eba991b52
jcranwang <jason.cr.wang@gmail.com>
2018-08-02 22:54:23 +0930

    Create redux store

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./index.css";
 import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
+import { searchRobot } from "./reducers";
 import "tachyons";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(searchRobot);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
